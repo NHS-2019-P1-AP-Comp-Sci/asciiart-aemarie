2ed2e77323c2a46b3476d79ad01b059cd8d76c75
s-jacintoa <s-jacintoa@53S500036641.it.bsd405.org>
2019-09-20 08:08:36 -0700

    Turning in project 1

---
+++ ASCIIArt/src/ascii_art.java
@@ -0,0 +1,31 @@
+
+public class ascii_art {
+	public static void main(String[] args) {
+		System.out.println("This is an image of a cherry about to be placed on top of two ice cream cones.");
+		cherry();
+		iceCream();
+	}	
+	public static void cherry() {
+		System.out.println("                       __.--~~.,-.__\\");
+		System.out.println("                      `~-._.-(`-.__`-.");
+		System.out.println("                             \\    `~~`\\");
+		System.out.println("                        .--./ \\");
+		System.out.println("                       /#   \\  \\.--.\\");
+		System.out.println("                       \\    /  /#   \\");
+		System.out.println("                        '--'   \\    /\\");
+		System.out.println("                                '--'\\");
+	}
+	public static void iceCream() {
+		System.out.println("______________________________________________");
+		System.out.println("______________/       \\__________/       \\__");
+		System.out.println("_____________/         \\________/         \\");
+		System.out.println("____________{           }______{           }");
+		System.out.println("_____________@@@@@@@@@@@________@@@@@@@@@@@");
+		System.out.println("______________@@@@@@@@@__________@@@@@@@@@");
+		System.out.println("_______________@@@@@@@____________@@@@@@@");
+		System.out.println("________________@@@@@______________@@@@@");
+		System.out.println("_________________@@@________________@@@");
+		System.out.println("__________________@__________________@");
+	}
+		
+}
